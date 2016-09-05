9dcb322a88be6cf32d152a6451b306efa37b7826
lsflp <lipeluiis@hotmail.com>
2016-09-05 12:15:09 -0300

    Arrumando erro do script

--- EP1/ep1_source/test.sh
+++ EP1/ep1_source/test.sh
@@ -4,11 +4,11 @@ echo "Executando com poucos processos"
 for j in {1..30}
 do
     ./ep1 1 t-1.txt t4-1.txt
-    ./capta 10 < t-4-1.txt >> t4-1a.txt
+    ./capta 10 < t4-1.txt >> t4-1a.txt
     ./ep1 2 t-1.txt t4-2.txt
-    ./capta 10 < t-4-2.txt >> t4-2a.txt
+    ./capta 10 < t4-2.txt >> t4-2a.txt
     ./ep1 3 t-1.txt t4-3.txt
-    ./capta 10 < t-4-3.txt >> t4-3a.txt  
+    ./capta 10 < t4-3.txt >> t4-3a.txt  
 done
 ./Transforma 10 < t4-1a.txt > t4-1b.txt
 ./Transforma 10 < t4-2a.txt > t4-2b.txt
@@ -17,11 +17,11 @@ echo "Executando com +/- processos"
 for j in {1..30}
 do
     ./ep1 1 t-2.txt t5-1.txt
-    ./capta 100 < t-5-1.txt >> t5-1a.txt
+    ./capta 100 < t5-1.txt >> t5-1a.txt
     ./ep1 2 t-2.txt t5-2.txt
-    ./capta 100 < t-5-2.txt >> t5-2a.txt
+    ./capta 100 < t5-2.txt >> t5-2a.txt
     ./ep1 3 t-2.txt t5-3.txt
-    ./capta 100 < t-5-3.txt >> t5-3a.txt  
+    ./capta 100 < t5-3.txt >> t5-3a.txt  
 done
 ./Transforma 100 < t5-1a.txt > t5-1b.txt
 ./Transforma 100 < t5-2a.txt > t5-2b.txt
@@ -30,11 +30,11 @@ echo "Executando com muitos processos"
 for j in {1..30}
 do
     ./ep1 1 t-3.txt t6-1.txt
-    ./capta 1000 < t-6-1.txt >> t6-1a.txt
+    ./capta 1000 < t6-1.txt >> t6-1a.txt
     ./ep1 2 t-3.txt t6-2.txt
-    ./capta 1000 < t-6-2.txt >> t6-2a.txt
+    ./capta 1000 < t6-2.txt >> t6-2a.txt
     ./ep1 3 t-3.txt t6-3.txt
-    ./capta 1000 < t-6-3.txt >> t6-3a.txt  
+    ./capta 1000 < t6-3.txt >> t6-3a.txt  
 done
 ./Transforma 1000 < t6-1a.txt > t6-1b.txt
 ./Transforma 1000 < t6-2a.txt > t6-2b.txt
